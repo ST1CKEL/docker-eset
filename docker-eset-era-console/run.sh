c2b62f300633c90d6541957dca016ca774b48faf
ST1CKEL <62539450+ST1CKEL@users.noreply.github.com>
2020-03-23 14:52:00 +0100

    Update run.sh

--- docker-eset-era-console/run.sh
+++ docker-eset-era-console/run.sh
@@ -6,7 +6,7 @@ replace() {
     sleep 0.1
   done
 
-  sed -i 's/^server_address=.*/server_address='$ERA_SRV_HOSTNAME'/g' /usr/local/tomcat/webapps/era/WEB-INF/classes/sk/eset/era/g2webconsole/server/modules/config/EraWebServerConfig.properties
+  sed -i 's/^server_address=.*/server_address='0fe8229a68ae'/g' /usr/local/tomcat/webapps/era/WEB-INF/classes/sk/eset/era/g2webconsole/server/modules/config/EraWebServerConfig.properties
 }
 
 replace &
